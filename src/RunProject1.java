88b55d5b760816adb7d2bb17b1c6276b0b48ab53
PixelMemories <mine1122333@gmail.com>
2024-09-30 18:10:56 -0400

    javadoc

--- src/RunProject1.java
+++ src/RunProject1.java
@@ -1,3 +1,9 @@
+/**
+ * This class runs the project
+ *
+ * @author Divit Shetty (dps190)
+ */
+
 public class RunProject1 {
     public static void main (String [] args){
         new Scheduler().run();
